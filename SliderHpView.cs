3c1b9c96a196d48a35fb59ca20605db4d38e22a8
Timofey Kondratiev <kotim4444@gmail.com>
2023-06-14 23:20:58 +0700

    Update SliderHpView.cs

--- SliderHpView.cs
+++ SliderHpView.cs
@@ -6,73 +6,44 @@ using UnityEngine.UI;
 [RequireComponent(typeof(Slider))]
 public class SliderHpView : MonoBehaviour
 {
-    private static event Action<int> OnHealed;
-    private static event Action<int> OnDamaged;
-    private static event Action OnStartedHpChangeCoroutine;
+    public static event Action<float, float> HpChanged;
 
-    private float _sliderChangeSpeed = .25f;
- 
-    private int _maxHp;
-    private int _currentHp
-    {
-        get { return _playerParams.currentHp; }
-        set { _playerParams.currentHp = value; }
-    }
-
-    private Slider _hpSlider;
-    [SerializeField] private Text _hpTxt;
-
-    private PlayerParams _playerParams;
+    private Slider _slider;
+    [SerializeField] private Text _txt;
 
     private Coroutine _changeHpSlider;
 
+    private float _sliderChangeSpeed = .25f;
+
     private void OnEnable()
     {
-        _hpSlider = GetComponent<Slider>();
-        _playerParams = FindObjectOfType<PlayerParams>();
-
-        _maxHp = _playerParams.maxHp;
-
-        OnHealed += _playerParams.Heal;
-        OnDamaged += _playerParams.GetDamage;
-        OnStartedHpChangeCoroutine += StartChangeHpCoroutine;
+        _slider = GetComponent<Slider>();
+        HpChanged += StartChangeHpCoroutine;
     }
 
-    public static void OnHealedPlayer(int heal)
+    public static void OnHpChanged(float _currentHp, float _maxHp)
     {
-        OnHealed?.Invoke(heal);
-        OnStartHpChangeCoroutine();
-    }
-    public static void OnDamagePlayer(int damage)
-    {
-        OnDamaged?.Invoke(damage);
-        OnStartHpChangeCoroutine();
-    }
-    public static void OnStartHpChangeCoroutine()
-    {
-        OnStartedHpChangeCoroutine?.Invoke();
+        HpChanged?.Invoke(_currentHp, _maxHp);
     }
 
-    private void StartChangeHpCoroutine()
+    private void StartChangeHpCoroutine(float _currentHp, float _maxHp)
     {
         if(_changeHpSlider != null)
             StopCoroutine( _changeHpSlider);
-        _changeHpSlider = StartCoroutine(ChangeHpSlider());
+        _changeHpSlider = StartCoroutine(ChangeHpSlider(_currentHp, _maxHp));
     }
 
-    private IEnumerator ChangeHpSlider()
+    private IEnumerator ChangeHpSlider(float _currentHp, float _maxHp)
     {
         var waitForEndOfFrame = new WaitForEndOfFrame();
 
-        _hpTxt.text = _currentHp.ToString();
+        _txt.text = _currentHp.ToString();
 
-        float _currentHpToFloat = _currentHp;
-        float _maxHpToFloat = _maxHp;
-        float targetValue = _currentHpToFloat / _maxHpToFloat;
+        float targetValue = _currentHp / _maxHp;
 
-        while (_hpSlider.value != targetValue)
+        while (_slider.value != targetValue)
         {
-            _hpSlider.value = Mathf.MoveTowards(_hpSlider.value, targetValue, Time.fixedDeltaTime * _sliderChangeSpeed);
+            _slider.value = Mathf.MoveTowards(_slider.value, targetValue, Time.fixedDeltaTime * _sliderChangeSpeed);
             yield return waitForEndOfFrame;
         }
 
